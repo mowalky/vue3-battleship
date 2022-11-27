ea598dd54fb75e676715edc024e18daf65404e96
mowalky <mowalky@outlook.com>
2022-11-26 22:11:16 -0600

    board setup

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,5 +1,26 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const boardSize = 10;
+
+function numtoletter(num: number): string {
+  return String.fromCharCode(96 + num);
+}
+</script>
 
 <template>
-  <div>board</div>
+  <div>
+    <div
+      v-for="row in boardSize"
+      key="row"
+      style="display: flex; background-color: pink"
+    >
+      {{ numtoletter(row) }}
+      <div
+        v-for="col in boardSize"
+        key="col"
+        style="flex: 1; width: 50px; height: 50px; border: 1px solid #ccc"
+      >
+        {{ col }}
+      </div>
+    </div>
+  </div>
 </template>
