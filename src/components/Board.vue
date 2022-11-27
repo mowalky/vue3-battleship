5cf0b3b115edac0650cba1ad594f6384917ab42b
mowalky <mowalky@outlook.com>
2022-11-27 15:49:15 -0600

    updated board

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -7,37 +7,12 @@ function numtoletter(num: number): string {
 </script>
 
 <template>
-  <div>
-    <div style="display: flex; margin-left: 52px">
-      <div
-        v-for="header in boardSize"
-        :key="header"
-        style="
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          width: 50px;
-          height: 50px;
-          border: 1px solid #ccc;
-        "
-      >
-        {{ header }}
-      </div>
-    </div>
+  <div
+    style="
+      background-image: url('https://st2.depositphotos.com/3768069/5426/i/950/depositphotos_54265481-stock-photo-water-seamless-texture-tile.jpg');
+    "
+  >
     <div v-for="row in boardSize" key="row" style="display: flex">
-      <div
-        :key="row"
-        style="
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          width: 50px;
-          height: 50px;
-          border: 1px solid #ccc;
-        "
-      >
-        {{ numtoletter(row) }}
-      </div>
       <div
         v-for="col in boardSize"
         :key="numtoletter(row) + col"
@@ -47,10 +22,10 @@ function numtoletter(num: number): string {
           align-items: center;
           width: 50px;
           height: 50px;
-          border: 1px solid #ccc;
+          border: 1px solid white;
         "
       >
-        x
+        {{ numtoletter(row) + col }}
       </div>
     </div>
   </div>
